569af3378f69b04abf70535a6dd7557780c872d4
bakkeby <bakkeby@gmail.com>
2024-07-11 22:38:58 +0200

    Adding launcher patch

--- dwm.c
+++ dwm.c
@@ -4111,6 +4111,7 @@ spawn(const Arg *arg)
 	if (arg->v == dmenucmd)
 		dmenumon[0] = '0' + selmon->num;
 	#endif // NODMENU_PATCH
+fprintf(stderr, "spawn running cmd:\n");
 
 	#if RIODRAW_PATCH
 	if ((pid = fork()) == 0)
