e54e7b49d122793431fc5c78ab1810ef51011315
Daniel Craig <dairy@queendairy.com>
2025-12-23 15:42:13 -0700

    removed taggrid patch.

--- patches.def.h
+++ patches.def.h
@@ -121,7 +121,7 @@
 /* This patch adds an option to place tags in rows like in many other window managers.
  * https://dwm.suckless.org/patches/taggrid/
  */
-#define BAR_TAGGRID_PATCH 1
+#define BAR_TAGGRID_PATCH 0
 
 /* Hover tag icons to see a preview of the windows on that tag.
  *
